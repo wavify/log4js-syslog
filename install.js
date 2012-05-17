1b27ed6d1c1d3526cfaef0cc4b6879bd9aee36a4
Jesse Sanford <jessesanford@gmail.com>
2012-05-17 19:29:20 -0400

    updated to make paths relative

--- install.js
+++ install.js
@@ -8,9 +8,17 @@ process.stdout.write("==========================================================
 
 //Symlink default strategies
 var appender_name = 'syslog';
+<<<<<<< HEAD
+var path_rel_to_node_modules = '../';
+var appenders_path = 'log4js/lib/appenders/';
+var appenders_path_rel_to_node_modules = "../../../";
+
+fs.symlinkSync(appenders_path_rel_to_node_modules + 'log4js-syslog' + './index.js', path_rel_to_node_modules + appenders_path + appender_name + '.js');
+=======
 var modules_path_rel_to_node_modules = '../';
 var appenders_path = 'log4js/lib/appenders/';
 
 fs.symlinkSync(fs.realpathSync('./index.js'), modules_path_rel_to_node_modules + appenders_path + appender_name + '.js');
+>>>>>>> 2e82f9ef43e9b9540ec86b14944d6ed69b32d56b
 
 process.stdout.write("Log4js-syslog Install: Finished.\n")
